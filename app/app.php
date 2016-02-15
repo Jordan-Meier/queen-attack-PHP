3dadcbdec4f5157ee73153b5cabb20c1914fccfd
Joseph Karasek and Jordan Meier <joseph.karasek@gmail.com>
2016-02-15 15:41:00 -0800

    add UI

--- app/app.php
+++ app/app.php
@@ -1,10 +1,24 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Queen.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/queen_attack", function() use ($app) {
+        $my_Queen = new Queen($_GET['qx_coord'], $_GET['qy_coord']);
+        $results = $my_Queen->canAttack($_GET['ix_coord'], $_GET['iy_coord']);
+        return $app['twig']->render('index.html.twig', array(
+            'result' => $results
+        ));
+
     });
 
     return $app;
